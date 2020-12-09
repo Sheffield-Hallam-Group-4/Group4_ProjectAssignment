0ef93f3ee5b9383309867c26b50ffd87b0f3cd0a
OLIVER BADGER <c0022288@hallam.shu.ac.uk>
2020-12-09 16:09:36 +0000

    Not sure wether its working or not

--- Pages/Customers/Login.cshtml.cs
+++ Pages/Customers/Login.cshtml.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Group_Project.Models;
+using Login_Session.Pages.DatabaseConnection;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +13,71 @@ namespace Group_Project1.Pages.Customers
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public Customer CustomerReg { get; set; }
+
+        public string Message { get; set; }
+
+        public string SessionID;
+
         public void OnGet()
         {
+
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+            DatabaseConnect dbstring = new DatabaseConnect(); //creating an object from the class
+            string DbConnection = dbstring.DatabaseString(); //calling the method from the class
+            Console.WriteLine(DbConnection);
+            SqlConnection conn = new SqlConnection(DbConnection);
+            conn.Open();
+
+            Console.WriteLine(CustomerReg.Email);
+            Console.WriteLine(CustomerReg.Password);
+
+
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = conn;
+                command.CommandText = @"SELECT CustomerName, CustomerLastName, Email FROM Customer WHERE Email = @Email AND Password = @PSWD";
+
+                command.Parameters.AddWithValue("@Email", CustomerReg.Email);
+                command.Parameters.AddWithValue("@PSWD", CustomerReg.Password);
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    CustomerReg.CustomerID = reader.GetString(0);
+                    CustomerReg.CustomerName = reader.GetString(1);
+                    CustomerReg.CustomerLastName = reader.GetString(2);
+                    CustomerReg.Email = reader.GetString(3);
+                }
+
+
+            }
+
+            if (!string.IsNullOrEmpty(CustomerReg.CustomerName))
+            {
+                SessionID = HttpContext.Session.Id;
+                HttpContext.Session.SetString("sessionID", SessionID);
+                HttpContext.Session.SetString("username", CustomerReg.Email);
+                HttpContext.Session.SetString("fname", CustomerReg.CustomerName);
+
+
+            }
+            else
+            {
+                Message = "Invalid Username and Password!";
+                return Page();
+            }
+            return RedirectToPage("./Index");
         }
     }
 }
